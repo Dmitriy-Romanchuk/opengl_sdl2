0f4b695d65df8ff75491dcd112be993f3309e8e1
Dmitriy-Romanchuk <roman4ukdimka@mail.ru>
2024-06-01 23:11:39 +0300

    Revert "Init commit for submodule"
    This reverts commit 577a4ce565e31621db35e77ffa34f9b7fdd94947.

--- src/GameObjects/Player.cpp
+++ src/GameObjects/Player.cpp
@@ -13,35 +13,28 @@ void Player::input(const SDL_Event& m_event)
 {
         if(m_event.type == SDL_KEYDOWN)
         {
-            auto sym = m_event.key.keysym.sym;
-            if(sym == SDLK_UP)
+            switch (m_event.key.keysym.sym)
             {
+            case SDLK_UP:
                 m_startPosition[1] += 0.005;
-            }
-
-            if(sym == SDLK_DOWN)
-            {
+                break;
+            case SDLK_DOWN:
                 m_startPosition[1] -= 0.005;
-            }
-
-            if(sym == SDLK_LEFT)
-            {
+                break;
+            case SDLK_LEFT:
                 m_startPosition[0] -= 0.005;
-            }
-
-            if(sym == SDLK_RIGHT)
-            {
+                break;
+            case SDLK_RIGHT:
                 m_startPosition[0] += 0.005;
-            }
-
-            if(sym == SDLK_KP_MINUS)
-            {
+                break;
+            case SDLK_KP_MINUS:
                 spriteSize -= 0.005;
-            }
-
-            if(sym == SDLK_KP_PLUS)
-            {
+                break;
+            case SDLK_KP_PLUS:
                 spriteSize += 0.005;
+                break;
+            default:
+                break;
             }
         }
 
